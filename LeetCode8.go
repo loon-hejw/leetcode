42427755e0d58d71c5ebf6d1999a3e9b99d168b3
hejw123 <54312752+hejw123@users.noreply.github.com>
2020-08-11 17:02:53 +0800

    leetcode8

--- LeetCode8.go
+++ LeetCode8.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+func myAtoi(str string) int {
+	atoiType , sum ,left  := 1 , 0 , 0
+	isNumber := func (b byte) bool {
+		if b >= '0' && b <= '9' {
+			return true
+		}
+		return false
+	}
+	// 去除空格
+	for left < len(str) && str[left] == ' ' { left ++ }
+
+	if left < len(str) && str[left] == '-' {
+		atoiType = -1 ; left ++
+	} else if left < len(str) && str[left] == '+' {
+		left ++
+	} else if left < len(str) && !isNumber(str[left]) {
+		return 0
+	}
+	for left < len(str) && isNumber(str[left]) {
+		sum = sum * 10 + int( str[left] - '0' )
+		if sum * atoiType <= math.MinInt32 {
+			return math.MinInt32
+		}
+		if sum * atoiType >= math.MaxInt32{
+			return math.MaxInt32
+		}
+		left ++
+	}
+	return sum * atoiType
+}
 func main() {
-	$END$
+	fmt.Println(myAtoi("-91283472332"))
 }
--- LeetCodeoffer11.go
+++ LeetCodeoffer11.go
@@ -3,7 +3,7 @@ package main
 func minArray(numbers []int) int {
 	left, right := 0, len(numbers)-1
 	for left < right {
-		mid := left + (right-left)>>1
+		mid := left + (right-left) >> 1
 		if numbers[mid] < numbers[right] {
 			right = mid
 		} else if numbers[mid] > numbers[right] {

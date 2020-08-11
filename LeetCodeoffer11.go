49e9fd5c857256688c59f1fa255fc564c2e821d8
hejw123 <54312752+hejw123@users.noreply.github.com>
2020-08-11 16:07:55 +0800

    除二操作 变为 位运算

--- LeetCodeoffer11.go
+++ LeetCodeoffer11.go
@@ -1,19 +1,19 @@
 package main
 
 func minArray(numbers []int) int {
-	left , right := 0 , len(numbers) - 1
+	left, right := 0, len(numbers)-1
 	for left < right {
-		mid := left + ( right - left ) / 2
+		mid := left + (right-left)>>1
 		if numbers[mid] < numbers[right] {
 			right = mid
 		} else if numbers[mid] > numbers[right] {
 			left = mid + 1
 		} else {
-			right --
+			right--
 		}
 	}
 	return numbers[left]
 }
 func main() {
-	
+
 }

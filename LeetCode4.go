ba351440285eb388feed26b4fd4eb56612948768
hejw123 <54312752+hejw123@users.noreply.github.com>
2020-09-08 19:28:09 +0800

    4题

--- LeetCode4.go
+++ LeetCode4.go
@@ -4,15 +4,50 @@ import "fmt"
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 
-	l1 , l2 := len(nums1) , len(nums2)
-	num := ( l1 + l2 ) >> 1
-	size := 0
-	for size < num {
-
+	totalLength := len(nums1) + len(nums2)
+	if totalLength % 2 == 1 {
+		midIndex := totalLength >> 1
+		return float64(getKthElement(nums1, nums2, midIndex + 1))
+	} else {
+		midIndex1, midIndex2 := totalLength >> 1 - 1, totalLength >> 1
+		return float64(getKthElement(nums1, nums2, midIndex1 + 1) + getKthElement(nums1, nums2, midIndex2 + 1)) / 2.0
 	}
-	fmt.Println(num)
-	return 1.0
+	return 0
+}
+
+func getKthElement(nums1, nums2 []int, k int) int {
+	index1, index2 := 0, 0
+	for {
+		if index1 == len(nums1) {
+			return nums2[index2 + k - 1]
+		}
+		if index2 == len(nums2) {
+			return nums1[index1 + k - 1]
+		}
+		if k == 1 {
+			return min(nums1[index1], nums2[index2])
+		}
+		half := k >> 1
+		newIndex1 := min(index1 + half, len(nums1)) - 1
+		newIndex2 := min(index2 + half, len(nums2)) - 1
+		pivot1, pivot2 := nums1[newIndex1], nums2[newIndex2]
+		if pivot1 <= pivot2 {
+			k -= (newIndex1 - index1 + 1)
+			index1 = newIndex1 + 1
+		} else {
+			k -= (newIndex2 - index2 + 1)
+			index2 = newIndex2 + 1
+		}
+	}
+	return 0
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
 func main() {
-	findMedianSortedArrays([]int{1,3},[]int{2})
+	fmt.Println(findMedianSortedArrays([]int{1,3},[]int{2}))
 }

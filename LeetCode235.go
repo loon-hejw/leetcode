8e6d215521fce6a9f9c171fea77a60b0bea8a25e
hejw <13552656607@163.com>
2020-09-28 10:20:56 +0800

    LeetCode235

---
+++ LeetCode235.go
@@ -0,0 +1,17 @@
+package main
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+		if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else if p.Val > root.Val && q.Val > root.Val {
+			root = root.Right
+		} else {
+			return root
+		}
+	}
+	return nil
+}
+func main() {
+	
+}
